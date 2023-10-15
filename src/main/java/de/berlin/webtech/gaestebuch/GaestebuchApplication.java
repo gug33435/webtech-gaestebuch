d021bfbb4b84b06400d56c4d54b0246dad53a5aa
Friedrich-Nicolas Puhl <puhlfriedrich@gmail.com>
2023-10-15 12:47:27 +0200

    init and Hello World

---
+++ src/main/java/de/berlin/webtech/gaestebuch/GaestebuchApplication.java
@@ -0,0 +1,13 @@
+package de.berlin.webtech.gaestebuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaestebuchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaestebuchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/berlin/webtech/gaestebuch/GaestebuchApplicationTests.java
@@ -0,0 +1,13 @@
+package de.berlin.webtech.gaestebuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaestebuchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
