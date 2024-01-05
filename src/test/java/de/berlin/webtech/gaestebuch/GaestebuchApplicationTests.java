38631d4049ed7c2017536a6c6c88930cb796ffb1
Friedrich-Nicolas Puhl <puhlfriedrich@gmail.com>
2024-01-05 20:23:04 +0100

    fix tests try #2

--- src/test/java/de/berlin/webtech/gaestebuch/GaestebuchApplicationTests.java
+++ src/test/java/de/berlin/webtech/gaestebuch/GaestebuchApplicationTests.java
@@ -3,10 +3,13 @@ package de.berlin.webtech.gaestebuch;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
 class GaestebuchApplicationTests {
 	@Test
 	void contextLoads() {
+		assertTrue(true);
 	}
 
 }
